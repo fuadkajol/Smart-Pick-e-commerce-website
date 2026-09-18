2842c00dd1b7ec90e234a2c55f13ebe656566962
Md Fuad Hassan kajol <fuad122k@gmail.com>
2026-09-18 11:52:15 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -202,7 +202,7 @@ export default function Footer() {
                   rel="noopener noreferrer"
                   className="hover:text-white transition-colors"
                 >
-                  Smartpickk
+                  Smartpick
                 </a>
               </li>
               <li>
